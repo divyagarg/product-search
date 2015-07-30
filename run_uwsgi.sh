39dbe5da2ee3362369f7af3ad484b7e99cc8aea7
Prince Mishra <indiandexter007@gmail.com>
2015-07-30 17:04:32 +0530

    tidying for staging

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,2 +1,1 @@
-#nohup /apps/.virtualenvs/product-search/bin/uwsgi --ini /apps/product-search/uwsgi.ini >product-search.out &
-nohup uwsgi --ini product_search_uwsgi.ini >/tmp/product-search.out &
+nohup uwsgi --ini /apps/product-search/product_search_uwsgi.ini >product-search.out &
