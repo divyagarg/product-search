3fd35aad631493d1c86ad681034f3d275cd5f73e
Prince Mishra <indiandexter007@gmail.com>
2015-07-31 14:39:30 +0530

    adding argument parser

--- app/api/products.py
+++ app/api/products.py
@@ -1,18 +1,26 @@
 import json
 import os
+
+from webargs import Arg
+from webargs.flaskparser import parser
 from random import shuffle
 
 from flask import request
-
 from app.api import api
 from app.decorators import jsonify, logrequest, logtime
+
+query_object_structure = {
+    'type' : Arg(str, required=True, validate=lambda t: t=='products'),
+    'filters' : Arg({
+        'q' : Arg(str, required=True)
+        }, required=True)
+}
 
 @api.route('/search/products/', methods = ['POST'])
 @jsonify
 @logrequest
 def products_search():
-    query = request.args.get('q')
-    params      = {}
+    params      = parser.parse(query_object_structure, request)
     f = open(os.path.join(os.getcwd(), 'data', 'product_search_results.json'), 'r')
     ret = json.loads(f.read())
     return ret
